f457680fc8b74413d602f5173f3cfe2960101d01
Matthew_GQ <qgan@email.arizona.edu>
2022-01-17 13:55:30 -0700

    the faceted_search

---
+++ faceted_search/index.js
@@ -0,0 +1,60 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+	SolrFacetedSearch,
+	SolrClient
+} from "solr-faceted-search-react";
+
+import solrReducer from "./solr-reducer";
+import { createStore } from "redux"
+
+// Create a store for the reducer.
+const store = createStore(solrReducer);
+
+// The search fields and filterable facets you want
+const fields = [
+	{label: "All text fields", field: "*", type: "text"},
+    {label: "Source", field: "source", type: "list-facet", facetSort:"index"},
+	{label: "Name", field: "name_t", type: "text"},
+	{label: "Characteristics", field: "characteristics_ss", type: "list-facet"},
+	{label: "Date of birth", field: "birthDate_i", type: "range-facet"},
+	{label: "Date of death", field: "deathDate_i", type: "range-facet"}
+];
+
+// The sortable fields you want
+const sortFields = [
+	{label: "Name", field: "koppelnaam_s"},
+	{label: "Date of birth", field: "birthDate_i"},
+	{label: "Date of death", field: "deathDate_i"}
+];
+
+// Construct the solr client api class
+const solrClient = 	new SolrClient({
+	url: "http://localhost:8983/solr/isb_rel/select",
+	searchFields: fields,
+	sortFields: sortFields,
+
+	// Delegate change callback to redux dispatcher
+	onChange: (state) => store.dispatch({type: "SET_SOLR_STATE", state: state})
+});
+
+// Register your app with the store
+store.subscribe(() =>
+	// In stead of using the handlers passed along in the onChange callback of SolrClient
+	// use the .getHandlers() method to get the default click / change handlers
+	ReactDOM.render(
+		<SolrFacetedSearch
+			{...store.getState()}
+			{...solrClient.getHandlers()}
+			bootstrapCss={true}
+			onSelectDoc={(doc) => console.log(doc)}
+		/>,
+		document.getElementById("app")
+	)
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+	// this will send an initial search initializing the app
+	solrClient.initialize();
+});
