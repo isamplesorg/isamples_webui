037dd7d69cda90d3d1fb9cdfa0939ae00a1f73e6
Matthew_GQ <qgan@email.arizona.edu>
2022-02-12 02:42:47 -0700

    add popup window and reorganize the app files.

---
+++ faceted_search/src/extension/popup.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class PopUp extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="popBox">
+        <ui>
+          <li>Using "<b>*</b>" for the wildcard search (eg. IGSN*).</li>
+          <li><b>&&</b> for the AND operator, <b>||</b> for the OR operator.</li>
+          <li>Using parenthesis "<b>()</b>" to group the multiple keywrods (eg. (water && great)).</li>
+          <li>If there are special characters (eg. : ) in the search words, Please put entire string in a quote, (eg. "IGSN:NHB0005J0").</li>
+        </ui>
+      </div>
+    )
+  }
+};
+
+export default PopUp;
