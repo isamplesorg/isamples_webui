a751ae91ee37f98ee658f8c2371947aebf8f96d7
Matthew_GQ <qgan@email.arizona.edu>
2022-02-15 16:09:28 -0700

    format check

--- src/extension/iSamples_resultList.js
+++ src/extension/iSamples_resultList.js
@@ -3,27 +3,27 @@ import React from "react";
 import cx from "classnames";
 
 class ResultList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { facet: "list" }
-	}
+  constructor(props) {
+    super(props);
+    this.state = { facet: "list" }
+  }
 
-	render() {
-		const { bootstrapCss } = this.props;
+  render() {
+    const { bootstrapCss } = this.props;
 
-		console.log(this.props.children[0].length !== 0 ? this.props.children[0][0]['props']['doc'] : [])
-		console.log(this.props.children[0].length !== 0 ? this.props.children[0][0]['props']['fields'] : [])
-		return (
-			<ul className={cx({ "list-group": bootstrapCss })}>
-				{this.props.children}
-			</ul>
-		);
-	}
+    console.log(this.props.children[0].length !== 0 ? this.props.children[0][0]['props']['doc'] : [])
+    console.log(this.props.children[0].length !== 0 ? this.props.children[0][0]['props']['fields'] : [])
+    return (
+      <ul className={cx({ "list-group": bootstrapCss })}>
+        {this.props.children}
+      </ul>
+    );
+  }
 }
 
 ResultList.propTypes = {
-	bootstrapCss: PropTypes.bool,
-	children: PropTypes.array
+  bootstrapCss: PropTypes.bool,
+  children: PropTypes.array
 };
 
 export default ResultList;
--- src/solr-reducer.js
+++ src/solr-reducer.js
@@ -1,16 +1,16 @@
 // src/solr-reducer.js
 const initialState = {
-	query: {},
-	result: {}
+  query: {},
+  result: {}
 }
 
 export default function performReducerAction(state = initialState, action) {
-	switch (action.type) {
-		case "SET_SOLR_STATE":
-			console.log("In reducer: ", action.state);
-			return { ...state, ...action.state }
-		default:
-			console.log("Unexepected action type, no action will be performed.")
-			return
-	}
+  switch (action.type) {
+    case "SET_SOLR_STATE":
+      console.log("In reducer: ", action.state);
+      return { ...state, ...action.state }
+    default:
+      console.log("Unexepected action type, no action will be performed.")
+      return
+  }
 }
