8248f73867beac1d07e022ced3f7bb958a2e3dc5
Matthew_GQ <qgan@email.arizona.edu>
2022-02-15 15:45:53 -0700

    add new features

---
+++ src/extension/iSamples_resultList.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React from "react";
+import cx from "classnames";
+
+class ResultList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {facet: "list"}
+  }
+
+  render() {
+    const {bootstrapCss} = this.props;
+
+    console.log(this.props.children[0].length !== 0 ? this.props.children[0][0]['props']['doc'] : [])
+    console.log(this.props.children[0].length !== 0 ? this.props.children[0][0]['props']['fields'] : [])
+    return (
+      <ul className={cx({"list-group": bootstrapCss})}>
+        {this.props.children}
+      </ul>
+    );
+  }
+}
+
+ResultList.propTypes = {
+  bootstrapCss: PropTypes.bool,
+  children: PropTypes.array
+};
+
+export default ResultList;
