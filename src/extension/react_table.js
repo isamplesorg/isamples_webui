929ffaaf9624630eeb411300e7c5193d6e2c257f
Matthew_GQ <qgan@email.arizona.edu>
2022-02-15 21:32:12 -0700

    Add higtlight and link for table

--- src/extension/react_table.js
+++ src/extension/react_table.js
@@ -1,5 +1,28 @@
 import React from "react";
 import { useTable } from "react-table";
+import parse from 'html-react-parser'
+
+// functional components to highlight search text and covert indentifers to the links
+// same function from iSamples_results.js
+const CellModify = (props) => {
+  const { field, value } = props
+  let text = [].concat(value || null).filter((v) => v !== null).join(", ");
+
+  if (field.type === 'text' && field.value !== undefined) {
+    // replace "&", "|", "(", ")", "*", "'", """ and duplicated whitespace with only one whitespace
+    const values = field.value.replaceAll(/&|\*|\(|\)|\||"|'/g, "").replaceAll(/\s+/g, " ").split(" ");
+
+    // split original text by search words insensitively by regex pattern
+    // g is for regex global and i is for insensitive.
+    values.map((value) => {
+      const regex = new RegExp(value, "gi");
+      text = text.split(regex).join("<span style='background-color:yellow;'>" + value + "</span>");
+      return text;
+    })
+  }
+
+  return field.field === 'id' ? <a href={"https://n2t.net/" + value}>{parse(text)}</a> : parse(text)
+}
 
 function Table(props) {
 
@@ -9,14 +32,20 @@ function Table(props) {
   // Setup data and columns for react table
   // We must use "data", "columns" variable names for the useTable
   // or it will throw an error
-  console.log(docs.map((record) => record['id']))
   const data = React.useMemo(
     () => docs,
     [docs]
   )
 
+  // https://react-table.tanstack.com/docs/api/useTable
+  // We could define cell functions to render different format result.
+  // Usage: Cell: Function | React.Component => JSX
   const columns = React.useMemo(
-    () => fields.map((field) => ({ Header: field.label, accessor: field.field })),
+    () => fields.map((field) => ({
+      Header: field.label,
+      accessor: field.field,
+      Cell: ({ value }) => <CellModify field={field} value={value} />
+    })),
     [fields]
   )
 
