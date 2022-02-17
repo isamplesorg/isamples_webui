0caeaed5bc2e43bde86420a5cca4375e4c163319
Matthew_GQ <qgan@email.arizona.edu>
2022-02-16 17:31:43 -0700

    add utilities file to store common functions.

--- src/extension/iSamples_results.js
+++ src/extension/iSamples_results.js
@@ -2,7 +2,7 @@
 import PropTypes from 'prop-types';
 import React from "react";
 import cx from "classnames";
-import parse from 'html-react-parser'
+import { ResultWrapper } from "./utilities";
 
 class iSamples_Result extends React.Component {
 
@@ -10,26 +10,6 @@ class iSamples_Result extends React.Component {
     const value = [].concat(doc[field] || null).filter((v) => v !== null);
 
     return value.join(", ");
-  }
-
-  // Highlight the search words in the result text.
-  HighlightWords(field, text) {
-    if (field.type === 'text' && field.value !== undefined) {
-      // replace "&", "|", "(", ")", "*", "'", """ and duplicated whitespace with only one whitespace
-      const values = field.value.replaceAll(/&|\*|\(|\)|\||"|'/g, "").replaceAll(/\s+/g, " ").split(" ");
-
-      // split original text by search words insensitively by regex pattern
-      // g is for regex global and i is for insensitive.
-      values.map((value) => {
-        const regex = new RegExp(value, "gi");
-        text = text.split(regex).join("<span style='background-color:yellow;'>" + value + "</span>");
-        return text;
-      })
-    }
-
-    // https://www.npmjs.com/package/html-react-parser
-    // Rather than using dangerouslySetInnerHTML. This library will convert HTML string to react elements
-    return parse(text);
   }
 
   render() {
@@ -41,13 +21,7 @@ class iSamples_Result extends React.Component {
           {fields.filter((field) => field.field !== "*" && field.collapse !== true).map((field, i) =>
             <li key={i}>
               <label>{field.label || field.field}</label>
-              {field.field === "id" ?
-                <a href={"https://n2t.net/" + this.renderValue(field.field, doc)}>
-                  {this.HighlightWords(field, this.renderValue(field.field, doc))}
-                </a>
-                :
-                this.HighlightWords(field, this.renderValue(field.field, doc))
-              }
+              <ResultWrapper field={field} value={this.renderValue(field.field, doc)} />
             </li>
           )}
         </ul>
