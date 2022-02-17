920856084a14dedf1b2ce00972d1355aa6044ad6
Matthew_GQ <qgan@email.arizona.edu>
2022-02-16 17:24:18 -0700

    make uilities file to store common functions.

--- src/extension/react_table.js
+++ src/extension/react_table.js
@@ -2,29 +2,7 @@
 
 import React from "react";
 import { useTable } from "react-table";
-import parse from 'html-react-parser'
-
-// functional components to highlight search text and covert indentifers to the links
-// same function from iSamples_results.js
-const CellModify = (props) => {
-  const { field, value } = props
-  let text = [].concat(value || null).filter((v) => v !== null).join(", ");
-
-  if (field.type === 'text' && field.value !== undefined) {
-    // replace "&", "|", "(", ")", "*", "'", """ and duplicated whitespace with only one whitespace
-    const values = field.value.replaceAll(/&|\*|\(|\)|\||"|'/g, "").replaceAll(/\s+/g, " ").split(" ");
-
-    // split original text by search words insensitively by regex pattern
-    // g is for regex global and i is for insensitive.
-    values.map((value) => {
-      const regex = new RegExp(value, "gi");
-      text = text.split(regex).join("<span style='background-color:yellow;'>" + value + "</span>");
-      return text;
-    })
-  }
-
-  return field.field === 'id' ? <a href={"https://n2t.net/" + value}>{parse(text)}</a> : parse(text)
-}
+import { ResultWrapper } from "./uilities";
 
 function Table(props) {
 
@@ -46,7 +24,7 @@ function Table(props) {
     () => fields.map((field) => ({
       Header: field.label,
       accessor: field.field,
-      Cell: ({ value }) => <CellModify field={field} value={value} />
+      Cell: ({ value }) => <ResultWrapper field={field} value={value} />
     })),
     [fields]
   )
