4cea51c20382ee46c5540273b54b904c016fe628
qgan7125 <qgan@email.arizona.edu>
2022-06-01 10:10:18 -0700

    modify auth

--- src/components/Login/auth.js
+++ src/components/Login/auth.js
@@ -1,16 +1,13 @@
 import { Navigate } from "react-router-dom";
-import Cookies from 'universal-cookie';
 
 function Auth() {
   // const curURL = window.location.href;
   // const url = new URL(curURL);
   // let searchParams = new URLSearchParams(url.hash.split('?')[1]);
-  const cookies = new Cookies();
 
-  const route = cookies.get('previousParams') ? "/main?" + new URLSearchParams(cookies.get('previousParams')).toString(): "/main";
   return (
     <>
-      {<Navigate to={route}/>}
+      {<Navigate to={"/main"} />}
     </>
   )
 }
--- src/components/navFooter.js
+++ src/components/navFooter.js
@@ -45,10 +45,10 @@ const FooterBar = function () {
           <div className="text-center text-muted">© Copyright 2020, iSamples Project.This material is based upon work
             supported
             by the National Science Foundation under Grant Numbers
-            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004839">2004839</a>,
-            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004562">2004562</a>,
-            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004642">2004642</a>, and
-            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004815">2004815</a>. Any opinions, findings, and
+            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004839"> 2004839</a>,
+            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004562"> 2004562</a>,
+            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004642"> 2004642</a>, and
+            <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=2004815"> 2004815</a>. Any opinions, findings, and
             conclusions or recommendations expressed in this material are those of the author(s) and do not
             necessarily reflect the views of the <a href="https://nsf.gov/">National Science Foundation</a>.</div>
         </div>
