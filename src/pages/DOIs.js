afe9da19f0e938d202934e2448aea15039b35d4d
qgan7125 <qgan@email.arizona.edu>
2022-06-23 13:59:38 -0400

    add default value

--- src/pages/DOIs.js
+++ src/pages/DOIs.js
@@ -38,7 +38,7 @@ function DOIs() {
     const { suffix, titles, creators, num_drafts, ...fields } = inputs;
     return {
       'orcid_id': cookie.get('orcid'),
-      "num_drafts": suffix ? 1 : (num_drafts),
+      "num_drafts": suffix ? 1 : (num_drafts || MIN_DRAFT),
       'datacite_metadata': {
         'data': {
           'type': 'dois',
