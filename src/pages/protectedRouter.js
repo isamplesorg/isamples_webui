a16f1c7f5b8cd2d88aeb3d020d83432233839f8e
qgan7125 <qgan@email.arizona.edu>
2022-06-23 14:49:47 -0400

    remove unused code

--- src/pages/protectedRouter.js
+++ src/pages/protectedRouter.js
@@ -7,18 +7,6 @@ import Cookies from 'universal-cookie';
 const ProtectedRoute = () => {
   const cookies = new Cookies();
 
-  const curURL = window.location.href;
-  const url = new URL(curURL);
-  const hash = url.hash;
-
-  // set a cookie for the main page parameters if the user is not authorized at the beginning.
-  if (hash.split('?').length >= 2) {
-
-    // read the parameters and convert the string to the object
-    const params = new URLSearchParams(url.hash.split("?")[1]);
-    cookies.set('previousParams', Object.fromEntries(params), { path: "/" });
-  }
-
   if (!cookies.get('access_token')) {
     return <Navigate to="/login" replace />;
   }
