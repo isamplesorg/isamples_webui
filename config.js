572e498a08acd1f1be60845c556caa1a1d2fa31b
qgan7125 <35419958+qgan7125@users.noreply.github.com>
2022-07-01 16:12:15 +0000

    Deploying to gh-pages from @ isamplesorg/isamples_webui@d36356544d9a5bdb9e3b5922c592b98e810a0301 🚀

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
   "solr_url": _server_base + "thing/select",
   "solr_stream": _server_base + "thing/stream",
   "thingpage": _server_base + "thingpage",
-  "dois_draft": _server_base + "mint_draft_identifiers",
+  "dois_draft": _server_base + "manage/mint_draft_identifiers",
   "login": _server_base + "manage/login",
   "analytics_src": "https://metrics.isample.xyz/js/plausible.js",
   "analytics_domain": "isamples.org",
