fed43fb8aead2b0595c6450459fc1ba8a0c67068
qgan7125 <qgan@email.arizona.edu>
2022-07-04 13:20:28 -0400

    add function to handle user info

--- src/pages/userInfo.js
+++ src/pages/userInfo.js
@@ -1,37 +1,40 @@
-import { useEffect } from "react";
-import Cookies from 'universal-cookie';
+import { useEffect, useState } from "react";
 import "css/userInfo.css";
 
 function UserInfo() {
-  const cookies = new Cookies();
+  const [info, setInfo] = useState({});
+
+
   useEffect(() => {
     console.log(window.config.userinfo)
-    fetch(window.config.userinfo, {
-      'method': 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': cookies.get('session'),
-      }
-    })
-      .then(res => res.json())
-      .then(res =>
-        console.log(res)
-      )
-  })
+    console.log(document.cookie)
+    const getInfo = async () => {
+      let res = await fetch(window.config.userinfo, {
+        'method': 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': undefined
+        },
+        credentials: 'include'
+      })
+      setInfo(res.json())
+    }
+    getInfo()
+  }, [])
 
   return (
     <>
       <div className="login">
         <div className="account-login">
           <dl>
-            <dt>Name:</dt><dd>test</dd>
-            <dt>orcid:</dt><dd>test</dd>
-            <dt>Issued:</dt><dd>test</dd>
-            <dt>Expires:</dt><dd>test</dd>
-            <dt>JWT:</dt><dd>test</dd>
+            <dt>Name:</dt><dd>{info['name'] || ""}</dd>
+            <dt>orcid:</dt><dd>{info['orcid'] || ""}</dd>
+            <dt>Issued:</dt><dd>{info['auth_time'] || ""}</dd>
+            <dt>Expires:</dt><dd>{info['expires_at'] || ""}</dd>
+            <dt>JWT:</dt><dd>{info['id_token'] || ""}</dd>
             <button
               className="btn btn-default"
-              onClick={() => navigator.clipboard.writeText('Copy this text to clipboard')}>
+              onClick={() => navigator.clipboard.writeText(info['id_token'] || "")}>
               Copy JWT
             </button>
           </dl>
