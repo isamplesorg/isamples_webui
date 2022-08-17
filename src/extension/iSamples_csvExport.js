4af04b11ef0627671739701254a27a85fa6576d6
Matthew_QG <qgan@email.arizona.edu>
2022-08-17 13:19:11 -0400

    Add loading animation and change filename

--- src/extension/iSamples_csvExport.js
+++ src/extension/iSamples_csvExport.js
@@ -8,29 +8,52 @@ const MAX_ROWS = 100000;
 
 const CsvExport = (props) => {
   const { bootstrapCss } = props;
-  const [downloadData, setDownloadData] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [collapse, setCollapse] = useState(false);
+  const [downloadData, setDownloadData] = useState("");
+  const [downloadStatus, setDownloadStatus] = useState("");
   const [formInfo, setFormInfo] = useState({
     start: 0,
     rows: MAX_ROWS
   })
   const csvRef = useRef();
 
+  // Generate filename based on the query parameters
   const filename = `${store.getState()['query']['searchFields']
     .filter(field => field.value)
     ?.map(field => field.value)
     .flat(2)
-    .concat([formInfo.start, formInfo.rows])
+    .concat([formInfo.start, store.getState()['results']['numFound'] > formInfo.rows ? formInfo.rows : store.getState()['results']['numFound']])
+    .map(e => {
+      if (typeof e === "object") {
+        return "bounding_box"
+      }
+
+      return e;
+    })
     .join("_")}.csv`;
+
 
   const fetchCsvResult = async () => {
     const { query } = store.getState();
-    const queryString = solrQuery.solrQuery({ ...query, rows: formInfo.rows, start: formInfo.start }, { wt: "csv" });
-    const API = `${query.url}?${queryString}`
+    const queryString = solrQuery.solrQuery({
+      ...query,
+      rows: formInfo.rows,
+      start: formInfo.start
+    }, { wt: "csv" });
+    const API = `${query.url}?${queryString}`;
+
+    setIsLoading(true);
+    setDownloadStatus("")
 
     fetch(API).then(res => res.text()).then(res => {
       setDownloadData(res);
-    })
+      setIsLoading(false);
+      setDownloadStatus("Download SuccessFul!")
+    },
+      err => {
+        setDownloadStatus("Download Failed!")
+      })
   }
 
   const handleClick = () => {
@@ -93,7 +116,7 @@ const CsvExport = (props) => {
             min={0}
             max={store.getState()['results']['numFound']}
             onChange={handleChange}
-            value={formInfo["start"] || 0}
+            value={formInfo["start"]}
           />
         </div>
         <div>
@@ -108,6 +131,11 @@ const CsvExport = (props) => {
             onChange={handleChange}
             value={formInfo["rows"] || MAX_ROWS} />
         </div>
+        <span><span className="glyphicon glyphicon-info-sign"></span> &nbsp; Record limit: 100000</span>
+        <div className="loadingExport" style={{ display: isLoading ? 'block' : "none" }}>
+          <div className="loadingTrack bg-primary"></div>
+        </div>
+        {downloadStatus}
         <button type="submit" className="btn btn-default pull-right btn-xs">Download</button>
       </form>
 
