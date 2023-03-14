166d86f1a35883203c2afb48344e964263f3a872
hyunssong <63157822+hyunssong@users.noreply.github.com>
2023-03-14 17:32:00 +0000

    Deploying to gh-pages from @ isamplesorg/isamples_webui@dc5448d7196c5b5330f78f68c57508641835eb9b 🚀

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // IMPORTANT: This file is only used during local development.  Any value in here needs to be
 // adapted to the Docker build process to work once the code is deployed.  If you make changes
 // here, you will need to make corresponding changes in the Docker build as well.
-const _server_base = "https://mars.cyverse.org/isamples_central/";
+const _server_base = "https://hyde.cyverse.org/isamples_central/";
 const DATACITE_PUBLISHER = ['Sesar', 'Geome', "OpenContext", "Smithsonian"];
 
 const config = {
@@ -17,6 +17,7 @@ const config = {
   "analytics_domain": "isamples.org",
   "datacite_prefix": "10.82273",
   "datacite_publisher": DATACITE_PUBLISHER,
+  "h3_count" : _server_base + "/h3_counts"
 };
 
 window.config = config;
