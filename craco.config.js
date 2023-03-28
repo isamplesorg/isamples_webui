5d250ae4f44ab5e129e9561fb64e449eee5ae9ca
hyunssong <hyunjusong@email.arizona.edu>
2023-03-28 13:32:48 -0700

    add simple unit test to check if works

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   plugins: [
     {
-      plugin: require("craco-cesium")()
+      plugin: require("craco-cesium")(),
     }
-  ]
+  ],
+  babel: {
+    "presets": [
+      ["@babel/preset-react", {
+      "runtime": "automatic"
+    }]],
+    "plugins": ["@babel/plugin-syntax-jsx"]
+  },
 };
