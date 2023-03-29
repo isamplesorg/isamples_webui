739859049e2dd2494e0fbc0c9a3a0081767a5533
Danny Mandel <danny.mandel@gmail.com>
2023-03-29 16:07:14 -0700

    Attempt to get new pyppeteer test running on CI

---
+++ pyppeteer_test.py
@@ -0,0 +1,31 @@
+import os
+import asyncio
+import time
+from pyppeteer import launch
+
+async def main():
+    # launch chromium browser in the background
+    browser = await launch(executablePath="/usr/bin/google-chrome-stable", headless=True)
+    # open a new tab in the browser
+    page = await browser.newPage()
+    filename = os.path.join(os.getcwd(), "build/index.html")
+    print(f"filename is {filename}")
+    file_url = f"file://{filename}"
+    print(f"going to open {file_url}")
+    # add URL to a new page and then open it
+    await page.goto(file_url)
+    time.sleep(10)
+    selector = "ul.solr-search-fields"
+    search_fields = await page.querySelector(selector)
+    print(f"search fields are {search_fields}")
+    if search_fields is None:
+        print("Did not find solr search fields, build is bad!")
+        raise SystemExit
+    else:
+        print("Success")
+    # close the browser
+    await browser.close()
+
+
+print("Starting...")
+asyncio.get_event_loop().run_until_complete(main())
