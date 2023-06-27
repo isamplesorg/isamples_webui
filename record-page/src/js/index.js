27eeebd3009b0f86976725037698c5ed66c3a1f5
Danny Mandel <danny.mandel@gmail.com>
2023-06-27 11:43:07 -0700

    Move the hypothesis code into the JS for the detail page

--- record-page/src/js/index.js
+++ record-page/src/js/index.js
@@ -1,3 +1,5 @@
+import Cookies from 'universal-cookie';
+
 const ZOOM_LEVEL = 6;
 const NO_ZOOM_LEVEL = 0;
 const DEFAULT_LOCATION = [0, 0];
@@ -189,3 +191,67 @@ function extractJsonldFromDOM() {
 };
 
 window.addEventListener('load', extractJsonldFromDOM());
+
+
+import Cookies from 'universal-cookie';
+
+async function fetchGrantToken() {
+  var grantToken = "";
+  // The story as of right now is that we only appear to have the "previousParams" session cookie showing up in
+  // this data structure.  If I go into Chrome, I can definitely see the session cookie, so something about how
+  // this is reading the cookies isn't getting that for some reason.
+  const cookies = new Cookies();
+  cookies.set('logged', true, { path: "/" });
+  console.log("cookies are ", cookies);
+  console.log("document cookies are ", document.cookie)
+  const sessionCookie = cookies.get("session");
+  console.log("session cookie is ", sessionCookie);
+  fetch("http://localhost:8000/manage/hypothesis_jwt", {
+    'method': 'POST',
+    credentials: "include",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': cookies.get('session'),
+    }
+  })
+    .then((res) => {
+      // check the POST return status
+      const { status } = res;
+      console.log("token response status is ", status);
+      if (status === 200) {
+        return res.text();
+      } else {
+        throw new Error(res);
+      }
+    })
+    .then((grantToken) => {
+      console.log("grant token is ", grantToken)
+      grantToken = grantToken.replace(/"/g, '');
+    },
+      (error) => {
+        console.warn(error);
+      }
+    )
+  console.log("grant token is ", grantToken);
+  window.hypothesisConfig = function () {
+    var hypothesisConfig = {
+        "services": [{
+          "apiUrl": "http://localhost:5000/api/",
+          "authority": "isample.xyz",
+          "onLoginRequest": function () {
+            window.location.assign(window.config.login);
+          },
+          "onLogoutRequest": function () {
+            window.location.assign(window.config.logout);
+          }
+        }]
+    };
+    if (grantToken !== "") {
+      hypothesisConfig["grantToken"] = grantToken;
+    }
+    return hypothesisConfig;
+  };
+}
+
+
+window.addEventListener('load', fetchGrantToken());
