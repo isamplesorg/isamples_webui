77511b400d4c541225a8a84d3712c2d58a658783
Hyunju(Sarah) Song <63157822+hyunssong@users.noreply.github.com>
2024-02-13 10:06:04 -0700

    Update hierarchy facet tests (#180)
    * Update test cases to only test CustomizedTree, and use mocked json for testing
    
    * Update comments and variable names of test cases
    
    * change function format

--- src/test/ContextFacet.test.js
+++ src/test/ContextFacet.test.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import HierarchyFacet from 'extension/iSamples_hierarchyFacet';
+import CustomizedTreeView from 'components/CV_hierarchy/hierarchy';
+
+function mockedHierarchyFunc (){
+    return {
+        "https://w3id.org/isample/vocabulary/sampledfeature/0.9/anysampledfeature": {
+          "label": {
+            "en": "Any sampled feature"
+          },
+          "children": [
+            {
+              "https://w3id.org/isample/vocabulary/sampledfeature/0.9/activehumanoccupationsite": {
+                "label": {
+                  "en": "Active human occupation site"
+                },
+                "children": []
+              }
+            },
+        ]
+    }
+    }
+}
 
 describe('ContextFacet', () => {
     it('should render the highest label of context hierarchy', () => {
-        const highestSpecimenLabel = "Any sampled feature"; // hardcoded value 
-        render(<HierarchyFacet label={"Context"} expanded={true} facets={[]} field={""}/>);
-        
-        const context = screen.getAllByText(highestSpecimenLabel);
+        const highestContextLabel = "Any sampled feature"; // hardcoded value
+        render(<CustomizedTreeView label={"Context"} value={[]} expanded={true} facetValues={["Any sampled feature", "Active human occupation site"]} facetCounts={[1000,100]} hierarchy={mockedHierarchyFunc} renderZeroCount={true}/>);
+ 
+        const context = screen.getAllByText(highestContextLabel);
         let contextTreeItem = null;
         // traverse and see if there is a one that has tree item as test id
         for ( let i = 0 ; i < context.length ; i++){
@@ -19,15 +39,15 @@ describe('ContextFacet', () => {
         expect(contextTreeItem).not.toBeNull();
     });
 
-    it('should render the extension label of context hierarchy', () => {
-        const extensionContextLabel = "Atmosphere";
-        render(<HierarchyFacet label={"Context"} expanded={true} facets={[]} field={""}/>);
+    it('should render the child label of context hierarchy', () => {
+        const childContextLabel = "Active human occupation site"; // hardcoded value
+        render(<CustomizedTreeView label={"Context"} value={[]} expanded={true} facetValues={["Any sampled feature", "Active human occupation site"]} facetCounts={[1000,100]} hierarchy={mockedHierarchyFunc} renderZeroCount={true}/>);
         const toggles = screen.getAllByTestId("tree-toggle");
         // expand toggles to see extensions
         for ( let i = 0; i< toggles.length ; i++ ){
             let toggle = toggles[i];
             fireEvent.click(toggle);
         }
-        expect(screen.getByText(extensionContextLabel)).toBeInTheDocument();
+        expect(screen.getByText(childContextLabel)).toBeInTheDocument();
     });
 })
