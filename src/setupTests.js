f8d95799ecada49caff0d7f3270294b026fbfbcf
Danny Mandel <danny.mandel@gmail.com>
2024-08-20 17:23:23 -0700

    Comment out setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,8 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
     // src/setupTests.js
     
-const enzyme = require('enzyme');
-const adapter_react = require('@wojtekmaj/enzyme-adapter-react-17');
-import '@testing-library/jest-dom'
+// const enzyme = require('enzyme');
+// const adapter_react = require('@wojtekmaj/enzyme-adapter-react-17');
+// import '@testing-library/jest-dom'
 
-enzyme.configure({ adapter: new adapter_react.Adapter() });
+// enzyme.configure({ adapter: new adapter_react.Adapter() });
